f2f31e7e13c947cfcf1c36310fb2edf930a329f7
JonyWinner <15685367981@163.com>
2021-08-22 12:06:27 +0800

    Add support for ShapeShift

--- main.c
+++ main.c
@@ -51,7 +51,7 @@ int main()
 			
 			printf("选择您要编译的系统：\n");
 			printf("A.pe B.peplus C.evo D.dotOS E.Arrow F.Crdroid G.Havoc\n");
-			printf("H.Project Sakura\n");
+			printf("H.Project Sakura I.ShapeShift \n");
 			printf("请选择：(输入序号)\n");
 			scanf("%c",&sys);
 	
@@ -65,6 +65,7 @@ int main()
 				case 'F' : system("./files/rom/get_crdroid.sh"); break;
 				case 'G' : system("./files/rom/get_havoc.sh"); break;
 				case 'H' : system("./files/rom/get_sakura.sh"); break;
+				case 'I' : system("./files/rom/get_ssos.sh"); break;
 				default : printf("暂不支持编译此rom!\n"); break;
 			}
 	
